abfb761fb35c552a823dcea0bab095ac4124462c
Bapu Pawar <bapupawar101@gmail.com>
2023-10-07 20:28:45 +0530

    - Initial commit

---
+++ TalentoTrack_FrontEnd/TalentoTrack_FrontEnd/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TalentoTrack_FrontEnd.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            TralentoTrack_API.TralentoTrackClient client = new TralentoTrack_API.TralentoTrackClient("https://localhost:7050/", new HttpClient());
+            var response = client.LoginAsync(new TralentoTrack_API.LoginRequest()
+            {
+                Username = username,
+                Password = password
+            }).Result;
+
+            if (response.Success)
+            {
+                ViewBag.message = "Login Successfully.";
+            }
+            else
+            {
+                ViewBag.message = response.ErrorMessage;
+            }
+
+            return View();
+        }
+    }
+}
