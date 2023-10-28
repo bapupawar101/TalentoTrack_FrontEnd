eae822e6a3676ea0f555b72e3dae8feff97402ac
Bapu Pawar <bapupawar101@gmail.com>
2023-10-28 13:17:47 +0530

    - Implemented authenticaton and authorization.

--- TalentoTrack_FrontEnd/TalentoTrack_FrontEnd/Controllers/AccountController.cs
+++ TalentoTrack_FrontEnd/TalentoTrack_FrontEnd/Controllers/AccountController.cs
@@ -1,34 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace TalentoTrack_FrontEnd.Controllers
 {
-    public class AccountController : Controller
-    {
-        public IActionResult Login()
+	public class AccountController : Controller
+	{
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+        public IActionResult Singout()
         {
-            return View();
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("login", "account");
         }
 
         [HttpPost]
-        public IActionResult Login(string username, string password)
-        {
-            TralentoTrack_API.TralentoTrackClient client = new TralentoTrack_API.TralentoTrackClient("https://localhost:7050/", new HttpClient());
-            var response = client.LoginAsync(new TralentoTrack_API.LoginRequest()
-            {
-                Username = username,
-                Password = password
-            }).Result;
+		public IActionResult Login(string username, string password)
+		{
+			TralentoTrack_API.TralentoTrackClient client = new TralentoTrack_API.TralentoTrackClient("https://localhost:7050/", new HttpClient());
+			var response = client.LoginAsync(new TralentoTrack_API.LoginRequest()
+			{
+				Username = username,
+				Password = password
+			}).Result;
 
-            if (response.Success)
-            {
-                ViewBag.message = "Login Successfully.";
-            }
-            else
-            {
-                ViewBag.message = response.ErrorMessage;
-            }
+			if (response.Success)
+			{
+				if (response.User != null)
+				{
+					var claims = new List<Claim>
+					{
+						new Claim(ClaimTypes.Name, username),
+						new Claim(ClaimTypes.NameIdentifier, response.User.Id.ToString()),
+						new Claim(ClaimTypes.Role, response.User.Role.ToString()),
+						new Claim("FullName", $"{response.User.FirstName} {response.User.LastName}")
+					};
 
-            return View();
-        }
-    }
+					var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+					var principal = new ClaimsPrincipal(identity);
+
+					HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+					return RedirectToAction("dashboard", "admin");
+				}
+			}
+			else
+			{
+				ViewBag.message = response.ErrorMessage;
+			}
+
+			return View();
+		}
+	}
 }
--- TalentoTrack_FrontEnd/TalentoTrack_FrontEnd/Program.cs
+++ TalentoTrack_FrontEnd/TalentoTrack_FrontEnd/Program.cs
@@ -1,16 +1,34 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Authentication
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+	.AddCookie(options =>
+	{
+		options.Cookie.Name = "TalentoTrack";
+		options.LoginPath = "/Account/Login";
+	});
+
+//Authorization
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("UserOnly", policy => policy.RequireRole("User"));
+});
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -18,10 +36,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+	name: "default",
+	pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
